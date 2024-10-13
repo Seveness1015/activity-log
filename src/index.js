0635f27541499485b37057313811858a0eeab7a5
許依茹 <132296367+Seveness1015@users.noreply.github.com>
2024-10-13 13:53:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { fetchAndFilterEvents } = require('./utils/github');
 const { updateReadme } = require('./utils/file');
 const { username, token, eventLimit, ignoreEvents, readmePath, commitMessage } = require('./config');
-const core = require('@actions/core');
+const core = require('@actions/core')
 
 // Main function to execute the update process
 async function main() {
@@ -9,8 +9,8 @@ async function main() {
         const activity = await fetchAndFilterEvents({ username, token, eventLimit, ignoreEvents });
         await updateReadme(activity, readmePath);
     } catch (error) {
-        core.setFailed(`❌ Error in the update process: ${error.message}`);
-        console.error(error);
+        core.setFailed(❌ Error in the update process: ${error.message});
+        console.error(error)
         process.exit(1);
     }
 }
